3766ba0fb2765a5ffb786cb6cf78d4d5a64be00e
Leandro Klem <leandrok.barcelos@gmail.com>
2025-06-08 13:11:53 -0300

    Criação da primeira versão do screper.py

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Extrair informações do glossário de termos do ATPCO, realizando a limpeza des
 
 ## Contexto
 O ATPCO é um ator central na indústria da aviação, fornecendo dados e padrões cruciais que permitem o funcionamento eficiente da precificação e distribuição de passagens aéreas globalmente. Seu glossário é útil para entendimento de termos utilizados pelo ATPCO e na indústria aéra como um todo, possibilitando melhor compreensão de seus significados e contextos.
- 
+
+## Libs Usadas
+- selenium: Para realizar a interação com o site dinâmico;
+- webdriver-manager: Para uso de web drivers sem a necessidade de download, instalação e atualizações manuais;
+- fake-useragent: Gerador de useragents aleatórios;
---
+++ src/screper.py
@@ -0,0 +1,65 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from fake_useragent import UserAgent
+import json
+print('xx')
+
+# User egent aleatório, evitando a detecção do robô
+ua = UserAgent(os='windows', browsers=['chrome'], min_percentage=2.0)
+user_agent = str(ua.random)
+        
+# Definição das opções do navegador
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_argument(f'user-agent={user_agent}')
+
+# Instânciação do web driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+# Realiza a busca da URL
+driver.get('https://atpco.net/glossary/')
+# Aguarda o carregamento da página
+driver.implicitly_wait(10)
+
+# Captura o elemento de seleção de registros por página
+record_selector = driver.find_element(By.NAME, 'records-per-page-selector')
+# Seleciona a opção "All" para exibir todos os registros
+record_selector.find_element(By.XPATH, '//option[@value="All"]').click()
+
+# Cria um dicionário para armazenar os termos do glossário
+glossary_terms = {}
+
+# Captura as opções de letra do glossário
+letter_buttons = driver.find_elements(By.CLASS_NAME, 'glossary-button')
+# Itera sobre cada botão de letra
+for button in letter_buttons:
+    # Clica no botão da letra para carregar os termos correspondentes
+    button.click()
+
+    # Aguarda o carregamento da página
+    driver.implicitly_wait(10)
+
+    # Cria um dicionário para armazenar os termos da letra atual
+    terms = {}
+    # Captura os elementos dos cards de glossário
+    glossary_cards = driver.find_elements(By.CLASS_NAME, 'definition-card')
+    # Itera sobre cada card de glossário
+    for card in glossary_cards:
+        # Captura o texto do título (termo) do card
+        term = card.find_element(By.CLASS_NAME, 'definition-title').text
+        # Captura o texto do conteúdo da definição do card
+        definition = card.find_element(By.CLASS_NAME, 'definition-main').text
+        # Adiciona o termo e sua definição ao dicionário de termos
+        terms[term] = definition
+
+    # Adiciona os termos da letra atual ao dicionário principal
+    glossary_terms[button.text] = terms
+
+# Cria um arquivo JSON para armazenar os termos do glossário
+glossary_terms_json = json.dumps(glossary_terms, indent=2, ensure_ascii=False)
+# Salva os termos do glossário em um arquivo JSON no diretório data
+with open('../data/raw_data_glossary.json', 'w', encoding='utf-8') as file:
+    file.write(glossary_terms_json)
+
