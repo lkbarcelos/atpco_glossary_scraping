d3e8380527e8aee0da0b160c2cca4bd1b94bbfc5
Leandro Klem <leandrok.barcelos@gmail.com>
2025-06-09 21:55:49 -0300

    Criação de feature de conversão dos JSONs para Markdown

---
+++ src/markdown.py
@@ -0,0 +1,30 @@
+import json
+
+# Lê os arquivos JSON de entrada e transforma em um dicionário
+with open('data/raw_data_glossary.json', 'r', encoding='utf-8') as f:
+    raw_data = dict(json.load(f))
+
+with open('data/translated_data_glossary.json', 'r', encoding='utf-8') as f:
+    translated_data = dict(json.load(f))
+
+# Consolida os dicionários em uma string
+consolidated_string = '# Glossário de Termos do ATPCO '
+for letter, terms in raw_data.items(): 
+    consolidated_string += f'\n\n## {letter.upper()} '
+    for term, definition in terms.items():
+            try:
+                # Acessa a definição traduzida do termo
+                translated_definition = translated_data[letter][term]['definition (pt-br)']
+                # Adiciona o termo e a definição à string consolidada
+                consolidated_string += f'\n### {term} '
+                consolidated_string += f'\n-**definition:**{definition} '
+                consolidated_string += f'\n-**definition (pt-br):**{translated_definition} \n'
+            except KeyError:
+                print(f'Erro ao acessar a chave "{term}" no dicionário translated_data.')
+
+# Salva a string consolidada em um arquivo Markdown
+with open('data/glossary_consolidated.md', 'w', encoding='utf-8') as f:
+    f.write(consolidated_string)
+    print('Glossário consolidado salvo em "data/glossary_consolidated.md".')
+
+
